9b8294480741e058e34b09dadfc37d4d38c42cfb
ckragh02 <connorkragh@umbc.edu>
2024-11-18 18:39:53 -0500

    Create gatherStarStats.py
    User provides a variable star using a known ID of the object. The program prints the equatorial coordinates in decimal degrees, a common identifier of the object in two formats, and the magnitude of the object. The program then gathers all of the stars within a user-provided radius, and picks the one closest in magnitude to the variable star to be a reference star. The program finally outputs the same information for the reference star as it did for the variable star.

---
+++ gatherStarStats.py
@@ -0,0 +1,106 @@
+from astroquery.simbad import Simbad
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+simbad = Simbad()
+simbad.add_votable_fields('ids')
+simbad.add_votable_fields('otype')
+simbad.add_votable_fields('flux(V)')
+
+
+varStar = "HIP 1213"
+
+# Returns the Variable Star's Coordinates and Catalog Number
+def getStarData(star=None):
+
+     resultTableStar=simbad.query_object(star)
+
+    # Ensures the star exists in SIMBAD, returns None, None if star is not found
+     if resultTableStar is None:
+            print(f"Source '{star}' not found in SIMBAD.")
+            return None, None # Return placeholders if the first source is not found
+     else:
+          # Finds the RA and Dec of the Variable Star in h:m:s and deg:':'', respectively
+          ra=resultTableStar["RA"][0]
+          dec=resultTableStar["DEC"][0]
+
+          ids = resultTableStar['IDS'][0]#.decode('utf-8')
+          catalog_ids = [id.strip() for id in ids.split('|')]
+          catalogNum = str(selectCatalog(catalog_ids))
+          if catalogNum == None:
+               catalogNum = resultTableStar["MAIN_ID"]
+          
+          catalogNumConv = catalogNum.replace(" ", "_")
+
+          # Converts RA and Dec into a SkyCoord object
+          skyCoordStar = SkyCoord(ra, dec, frame="icrs", unit=(u.hourangle, u.deg))
+
+          magnitudeV = resultTableStar["FLUX_V"][0]
+          
+          return skyCoordStar, catalogNum, catalogNumConv, magnitudeV # Returns RA and Dec in decimal degrees
+
+# Uses simbad.query_region() to find stars that could be used as references within the same frame (+-1 magnitude ideally)
+def getPotentialReferences(coordinates):
+          
+     potentialRef=simbad.query_region(coordinates, radius="0d8m")
+     
+     star_results = potentialRef[potentialRef['OTYPE'] == 'Star']
+
+     return star_results
+
+# Selects a catalog number from a list of catalogs an object belongs to
+# Hierarchy:
+# HD -> HIP -> HIC -> 2MASS
+
+def selectCatalog(catalogs):
+    
+    # Loops through list of catalogs and searches for each one in the hierarchy
+    for entry in catalogs:
+         if(entry.startswith("HD")):
+              return entry
+    for entry in catalogs:
+         if(entry.startswith("HIP")):
+              return entry
+    for entry in catalogs:
+         if(entry.startswith("HIC")):
+              return entry
+    for entry in catalogs:
+         if(entry.startswith("2MASS")):
+              return entry
+         
+    return 
+
+# Finds the coordinates of a given variable star
+originCoords, varID, varIDConv, varMagV,  = getStarData(varStar if varStar else None)
+
+if(originCoords!=None and varID!=None):
+
+    varRA = originCoords.ra.deg
+    varDec = originCoords.dec.deg
+
+    print(varRA)
+    print(varDec)
+    print(varID)
+    print(varIDConv)
+    print(varMagV)
+
+    refComp = 100
+    references = getPotentialReferences(originCoords)
+    if references is not None:
+         #for row in references:
+              #print(f"MAIN_ID: {row['MAIN_ID']}, FLUX_V: {row['FLUX_V']}")        
+          for row in references:
+              if abs(varMagV-row['FLUX_V'] < refComp):
+                   refName=row['MAIN_ID']
+
+    refCoords, refID, refIDConv, refMagV = getStarData(refName if refName else None)
+    refRA = refCoords.ra.deg
+    refDec = refCoords.dec.deg
+
+    print(refRA)
+    print(refDec)
+    print(refID)
+    print(refIDConv)
+    print(refMagV)
+
+
