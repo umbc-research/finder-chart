3391e9fdac6ca8a76663ffd58b94d1be88de1a20
ckragh02 <connorkragh@umbc.edu>
2024-11-19 14:57:29 -0500

    InterruptedProgress
    All needed info gathered, working on collecting based on given list of sources but interrupted by class time.

--- gatherStarStats.py
+++ gatherStarStats.py
@@ -3,32 +3,25 @@ from astroquery.simbad import Simbad
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 
-# List of targets that need ID's and Reference Stars
-targetList = "C:\\Users\\conno\\Downloads\\VariableStarsTargets_TargetCommons.csv"
-
-df=pd.read_csv(targetList)
-print(df)
-
 simbad = Simbad()
 simbad.add_votable_fields('ids')
 simbad.add_votable_fields('otype')
 simbad.add_votable_fields('flux(V)')
 
-
-
-
-varStar = "HIP 1213"
+#varStar = "HIP 1213"
 
 # Returns the Variable Star's Coordinates and Catalog Number
 def getStarData(star=None):
 
-     resultTableStar=simbad.query_object(star)
+     
 
     # Ensures the star exists in SIMBAD, returns None, None if star is not found
-     if resultTableStar is None:
+     if star == None:
             print(f"Source '{star}' not found in SIMBAD.")
-            return None, None # Return placeholders if the first source is not found
+            return None, None, None, None, None # Return placeholders if the first source is not found
      else:
+          resultTableStar=simbad.query_object(star)
+
           # Finds the RA and Dec of the Variable Star in h:m:s and deg:':'', respectively
           ra=resultTableStar["RA"][0]
           dec=resultTableStar["DEC"][0]
@@ -42,16 +35,16 @@ def getStarData(star=None):
           catalogNumConv = catalogNum.replace(" ", "_")
 
           # Converts RA and Dec into a SkyCoord object
-          skyCoordStar = SkyCoord(ra, dec, frame="icrs", unit=(u.hourangle, u.deg))
+          #skyCoordStar = SkyCoord(ra, dec, frame="icrs", unit=(u.hourangle, u.deg))
 
           magnitudeV = resultTableStar["FLUX_V"][0]
           
-          return skyCoordStar, catalogNum, catalogNumConv, magnitudeV # Returns RA and Dec in decimal degrees
+          return ra, dec, catalogNum, catalogNumConv, magnitudeV # Returns RA and Dec in decimal degrees
 
 # Uses simbad.query_region() to find stars that could be used as references within the same frame (+-1 magnitude ideally)
 def getPotentialReferences(coordinates):
           
-     potentialRef=simbad.query_region(coordinates, radius="0d8m")
+     potentialRef=simbad.query_region(coordinates, radius="0d6m")
      
      star_results = potentialRef[potentialRef['OTYPE'] == 'Star']
 
@@ -77,39 +70,117 @@ def selectCatalog(catalogs):
          if(entry.startswith("2MASS")):
               return entry
          
-    return 
+    return
 
-# Finds the coordinates of a given variable star
-originCoords, varID, varIDConv, varMagV,  = getStarData(varStar if varStar else None)
-
-if(originCoords!=None and varID!=None):
-
-    varRA = originCoords.ra.deg
-    varDec = originCoords.dec.deg
-
-    print(varRA)
-    print(varDec)
-    print(varID)
-    print(varIDConv)
-    print(varMagV)
-
-    refComp = 100
-    references = getPotentialReferences(originCoords)
-    if references is not None:
-         #for row in references:
-              #print(f"MAIN_ID: {row['MAIN_ID']}, FLUX_V: {row['FLUX_V']}")        
-          for row in references:
-              if abs(varMagV-row['FLUX_V'] < refComp):
-                   refName=row['MAIN_ID']
-
-    refCoords, refID, refIDConv, refMagV = getStarData(refName if refName else None)
-    refRA = refCoords.ra.deg
-    refDec = refCoords.dec.deg
-
-    print(refRA)
-    print(refDec)
-    print(refID)
-    print(refIDConv)
-    print(refMagV)
+# Separates hh mm and ss or deg ' and '' from one string into three strings
+def extractAngle(coordinates):
+     coordSeparated = [ang.strip() for ang in coordinates.split(' ')]
+     # Removes "+" from Dec degrees (Prevents equations in Google Sheets)
+     decSoln = coordSeparated[0].replace("+", "")
+     print(coordSeparated)
+     return decSoln, coordSeparated[1], coordSeparated[2]
 
 
+# List of targets that need ID's and Reference Stars
+targetList = "C:\\Users\\conno\\Downloads\\VariableStarsTargets_TargetCommons.csv"
+
+inputDF = pd.read_csv(targetList)
+#print(commonVarNames)
+commonVarNames = []
+for i in inputDF.itertuples():
+     commonVarNames.append(i[1])
+
+# Variable Star IDs
+varIDs = []
+# Variable Stars IDs with underscores instead of whitespaces
+varIDsConv = []
+# Reference Star IDs
+refIDs = []
+# Reference Star IDs with underscores instead of whitespaces
+refIDsConv = []
+# Reference RA hours
+refRAh = []
+# Reference RA minutes
+refRAm = []
+# Reference RA seconds
+refRAs = []
+# Reference Dec degrees
+refDecd = []
+# Reference Dec arcminutes
+refDecam = []
+# Reference Dec arcseconds
+refDecas = []
+# Reference Epoch
+refEpochs = []
+# Reference Star V Magnitudes
+refMagsV = []
+
+
+
+for target in commonVarNames:
+
+     # Finds the coordinates of a given variable star
+
+     #varRA, varDec, varID, varIDConv, varMagV  = getStarData(varStar if varStar else None)
+     varRA, varDec, varID, varIDConv, varMagV  = getStarData(target if target else None)
+
+     if(varRA != None and varDec != None):
+
+          originCoords = SkyCoord(varRA, varDec, frame="icrs", unit=(u.hourangle, u.deg))
+
+          if(originCoords!=None and varID!=None):
+
+     #    print(varRA)
+     #    print(varDec)
+     #    print(varID)
+     #    print(varIDConv)
+     #    print(varMagV)
+
+               refComp = 100
+               references = getPotentialReferences(originCoords)
+               if references is not None:
+                    #for row in references:
+                         #print(f"MAIN_ID: {row['MAIN_ID']}, FLUX_V: {row['FLUX_V']}")        
+                         for row in references:
+                              if abs(varMagV-row['FLUX_V']) < refComp:
+                                   refComp = abs(varMagV-row['FLUX_V'])
+                                   refName=row['MAIN_ID']
+
+               refRA, refDec, refID, refIDConv, refMagV = getStarData(refName if refName else None)
+               print(target)
+               # Splits RA and Dec into hms and deg arcmin arcsec
+               refSplitRA = extractAngle(refRA)
+               refSplitDec = extractAngle(refDec)
+
+               # Populates the current row with current target's info
+               varIDs.append(varID)
+               varIDsConv.append(varIDConv)
+               refIDs.append(refID)
+               refIDsConv.append(refIDConv)
+               refRAh.append(refSplitRA[0])
+               refRAm.append(refSplitRA[1])
+               refRAs.append(refSplitRA[2])
+               refDecd.append(refSplitDec[0])
+               refDecam.append(refSplitDec[1])
+               refDecas.append(refSplitDec[2])
+               refEpochs.append("J2000")
+               refMagsV.append(refMagV)
+          
+          else:
+               # Populates the current row with current target's info
+               varIDs.append(None)
+               varIDsConv.append(None)
+               refIDs.append(None)
+               refIDsConv.append(None)
+               refRAh.append(None)
+               refRAm.append(None)
+               refRAs.append(None)
+               refDecd.append(None)
+               refDecam.append(None)
+               refDecas.append(None)
+               refEpochs.append(None)
+               refMagsV.append(None)
+
+
+df = pd.DataFrame({"Object Name": varIDsConv, "Catalog No.": varIDs ,"Reference Star Object Name": refIDsConv, "Reference Star Catalog No.": refIDs, "hh": refRAh, "mm": refRAm, "ss": refRAs, "deg": refDecd, "'": refDecam, "''": refDecas, "Reference Epoch": refEpochs, "Reference Star Magnitude (V)": refMagsV})
+df.to_csv("VariablesExtraInfo.csv", index=False)
