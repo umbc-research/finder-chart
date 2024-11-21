5bcb7fe9d13ae78b0dfd88c94b419858c3228ef0
ckragh02 <connorkragh@umbc.edu>
2024-11-21 13:22:57 -0500

    Finder chart annotations
    Variable and Reference stars are automatically labeled now on the chart

--- getFinderCharts.py
+++ getFinderCharts.py
@@ -25,13 +25,18 @@ def extractAngle(coordinates):
      else:
           return None, None, None
 
-star = "U Aql"
-angFoVstr = "0d4m"
-angFoV = 4 # arcminutes
+textOffset = 0.01
+star = "SY Cas"
+reference = "2MASS J00144120+5827154"
+angFoVstr = "0d8m"
+angFoV = 8 # arcminutes
 
 starTable = simbad.query_object(star)
+referenceTable = simbad.query_object(reference)
 
 centerCoords = getCenterCoords(starTable)
+referenceCoords = getCenterCoords(referenceTable)
+
 #print(centerCoords.ra.deg)
 surroundings = simbad.query_region(centerCoords, angFoVstr)
 
@@ -44,16 +49,16 @@ surroundingsMag = []
 for i in surroundings:
     #print(i["MAIN_ID"])
         starLoc=getCenterCoords(i)
+
+        if isinstance(i["FLUX_V"], np.float32):
+            adjMag = 5/math.log(i["FLUX_V"])     
+        else:
+            adjMag = 1
+    
         surroundingsNames.append(i["MAIN_ID"])
         surroundingsRA.append(starLoc.ra.deg)
         surroundingsDec.append(starLoc.dec.deg)
-
-        if isinstance(i["FLUX_V"], np.float32):
-            adjMag = 10/math.log(i["FLUX_V"])     
-        else:
-            adjMag = 1
-        
-        surroundingsMag.append(pow(adjMag,2))
+        surroundingsMag.append(pow(adjMag,3))
 
 raArray = np.array(surroundingsRA)
 
@@ -62,7 +67,8 @@ sizeArray = np.array(surroundingsMag)
 
 plt.scatter(raArray, decArray, s=sizeArray, color="black")
 
-
+plt.annotate("Variable", [centerCoords.ra.deg, centerCoords.dec.deg], [centerCoords.ra.deg+textOffset, centerCoords.dec.deg+textOffset], arrowprops=dict(facecolor="black", headwidth=0.5, width=0.5, headlength=0.5))
+plt.annotate("Reference", [referenceCoords.ra.deg, referenceCoords.dec.deg], [referenceCoords.ra.deg+textOffset, referenceCoords.dec.deg+textOffset], arrowprops=dict(facecolor="black", headwidth=0.5, width=0.5, headlength=0.5))
 
 plt.xlim(centerCoords.ra.deg+(angFoV/60),centerCoords.ra.deg-(angFoV/60))
 plt.ylim(centerCoords.dec.deg-(angFoV/60), centerCoords.dec.deg+(angFoV/60))
