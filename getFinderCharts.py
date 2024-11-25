79caee4f3afd5eeb71d1c85c73fc0b8fa3e2b19e
ckragh02 <connorkragh@umbc.edu>
2024-11-25 11:29:10 -0500

    Added Comments

--- getFinderCharts.py
+++ getFinderCharts.py
@@ -10,11 +10,16 @@ import math
 simbad=Simbad()
 simbad.add_votable_fields("flux(V)")
 
+# Input a Simbad object from simbad.query_object
+# Output a SkyCoord object
 def getCenterCoords(objTable=None):
     objRA, objDec = objTable["RA"], objTable["DEC"]
     center = SkyCoord(objRA, objDec, frame="icrs", unit=(u.hourangle, u.deg))
     return center
 
+
+# Input coordinates in string of format (+deg ' '')
+# Output each individual coordinate as its own variable
 def extractAngle(coordinates):
      coordSeparated = [ang.strip() for ang in coordinates.split(' ')]
 
@@ -26,10 +31,17 @@ def extractAngle(coordinates):
      else:
           return None, None, None
 
+# Manually input star and reference here
+
 star = "SY Cas"
 reference = "2MASS J00144120+5827154"
+
+# Manually input FoV here
+
 angFoVstr = "0d8m"
 angFoV = 8 # arcminutes
+
+# Set up vertical offset for labelling stars
 textOffset = angFoV/300
 
 starTable = simbad.query_object(star)
@@ -38,8 +50,11 @@ referenceTable = simbad.query_object(reference)
 centerCoords = getCenterCoords(starTable)
 referenceCoords = getCenterCoords(referenceTable)
 
-#print(centerCoords.ra.deg)
+# Find stars in a given region
+
 surroundings = simbad.query_region(centerCoords, angFoVstr)
+
+ # Initialize star info lists
 
 surroundingsTable = []
 surroundingsNames = []
@@ -47,31 +62,45 @@ surroundingsRA = []
 surroundingsDec = []
 surroundingsMag = []
 
-for i in surroundings:
-    #print(i["MAIN_ID"])
-        starLoc=getCenterCoords(i)
+# Organize info of all stars into lists
 
-        if isinstance(i["FLUX_V"], np.float32):
-            adjMag = 5/math.log(i["FLUX_V"])     
-        else:
-            adjMag = 1
+for i in surroundings:
+    starLoc=getCenterCoords(i)
+
+    # If a star magnitude is known, scale it,
+    # Else, make the size of the point equal to 1
+        
+    if isinstance(i["FLUX_V"], np.float32):
+        adjMag = 5/math.log(i["FLUX_V"])     
+    else:
+        adjMag = 1
     
-        surroundingsNames.append(i["MAIN_ID"])
-        surroundingsRA.append(starLoc.ra.deg)
-        surroundingsDec.append(starLoc.dec.deg)
-        surroundingsMag.append(pow(adjMag,3))
+    # Initializes lists of info
+    surroundingsNames.append(i["MAIN_ID"])
+    surroundingsRA.append(starLoc.ra.deg)
+    surroundingsDec.append(starLoc.dec.deg)
+    surroundingsMag.append(pow(adjMag,3))
+
+# Translates lists of star info into arrays to be plotted
 
 raArray = np.array(surroundingsRA)
-
 decArray = np.array(surroundingsDec)
 sizeArray = np.array(surroundingsMag)
 
+# Plots the known stars in the field
+
 plt.scatter(raArray, decArray, s=sizeArray, color="black")
+
+# Labels both the variable and the reference star
 
 plt.annotate("Variable", [centerCoords.ra.deg, centerCoords.dec.deg], [centerCoords.ra.deg, centerCoords.dec.deg+textOffset], horizontalalignment='center', arrowprops=dict(arrowstyle='-'))
 plt.annotate("Reference", [referenceCoords.ra.deg, referenceCoords.dec.deg], [referenceCoords.ra.deg, referenceCoords.dec.deg+textOffset], horizontalalignment='center', arrowprops=dict(arrowstyle='-'))
 
+# Sets the figure Title to the name of the Variable Star 
+
 plt.title(star)
+
+# Sets the axis labels and ranges
 
 plt.xlabel("RA (deg)")
 plt.xlim(centerCoords.ra.deg+(angFoV/60),centerCoords.ra.deg-(angFoV/60))
@@ -79,12 +108,17 @@ plt.xlim(centerCoords.ra.deg+(angFoV/60),centerCoords.ra.deg-(angFoV/60))
 plt.ylabel("Dec (deg)")
 plt.ylim(centerCoords.dec.deg-(angFoV/60), centerCoords.dec.deg+(angFoV/60))
 
+# Adds a scalebar to the figure
+
 ax = plt.gca()
 scalebar = AnchoredSizeBar(ax.transData, 1/60, "1 minute", loc='lower left', pad=1, frameon=False)
 
 ax.add_artist(scalebar)
 
+# Either display or save image here (Comment out to exclude)
+
 #plt.show()
+
 path = "FinderChartPNGs\\finder_"
 fileName = path+star.replace(" ", "_")
 
